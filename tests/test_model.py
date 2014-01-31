d9757da76be06accbc22b83d97478fdcb8f778af
Steve Canny <scanny@cisco.com>
2014-01-30 23:20:31 -0800

    reorg: reorganize fixtures in test_model.py
    No code changes, just bringing fixtures into test class to improve
    readability.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-#
-# test_model.py
-#
-# Copyright (C) 2013 Steve Canny scanny@cisco.com
-#
-# This module is part of opc-diag and is released under the MIT License:
-# http://www.opensource.org/licenses/mit-license.php
 
-"""Unit tests for model module"""
+"""
+Unit tests for model module
+"""
 
 from __future__ import unicode_literals
 
@@ -27,101 +22,6 @@ from .unitutil import class_mock, instance_mock
 
 DIRPATH = 'dirpath'
 PACKAGE_PATH = 'package_path'
-
-
-@pytest.fixture
-def blob_(request):
-    return instance_mock(str, request)
-
-
-@pytest.fixture
-def blob_2_(request):
-    return instance_mock(str, request)
-
-
-@pytest.fixture
-def blob_collection_(request, uri_, uri_2_, blob_, blob_2_):
-    return {uri_: blob_, uri_2_: blob_2_}
-
-
-@pytest.fixture
-def Package_(request):
-    Package_ = class_mock('opcdiag.model.Package', request)
-    return Package_
-
-
-@pytest.fixture
-def path_(request):
-    return instance_mock(str, request)
-
-
-@pytest.fixture
-def PhysPkg_(request, phys_pkg_):
-    PhysPkg_ = class_mock('opcdiag.model.PhysPkg', request)
-    PhysPkg_.read.return_value = phys_pkg_
-    return PhysPkg_
-
-
-@pytest.fixture
-def phys_pkg_(request, root_uri_, uri_, uri_2_, blob_, blob_2_):
-    phys_pkg = instance_mock(PhysPkg, request)
-    phys_pkg.root_uri = root_uri_
-    phys_pkg.__iter__.return_value = iter(((uri_,   blob_),
-                                           (uri_2_, blob_2_)))
-    return phys_pkg
-
-
-@pytest.fixture
-def PkgItem_(request, pkg_item_, pkg_item_2_):
-    PkgItem_ = class_mock('opcdiag.model.PkgItem', request)
-    PkgItem_.side_effect = [pkg_item_, pkg_item_2_]
-    return PkgItem_
-
-
-@pytest.fixture
-def pkg_item_(request, blob_):
-    pkg_item_ = instance_mock(PkgItem, request)
-    pkg_item_.blob = blob_
-    pkg_item_.is_rels_item = True
-    pkg_item_.is_xml_part = True
-    return pkg_item_
-
-
-@pytest.fixture
-def pkg_item_2_(request, blob_2_):
-    pkg_item_2_ = instance_mock(PkgItem, request)
-    pkg_item_2_.blob = blob_2_
-    pkg_item_2_.is_rels_item = False
-    pkg_item_2_.is_xml_part = False
-    return pkg_item_2_
-
-
-@pytest.fixture
-def pkg_item_3_(request):
-    pkg_item_3_ = instance_mock(PkgItem, request)
-    pkg_item_3_.is_rels_item = True
-    pkg_item_3_.is_xml_part = True
-    return pkg_item_3_
-
-
-@pytest.fixture
-def pkg_item_dict_(request, uri_, uri_2_, pkg_item_, pkg_item_2_):
-    return {uri_: pkg_item_, uri_2_: pkg_item_2_}
-
-
-@pytest.fixture
-def root_uri_(request):
-    return instance_mock(str, request)
-
-
-@pytest.fixture
-def uri_(request):
-    return instance_mock(str, request)
-
-
-@pytest.fixture
-def uri_2_(request):
-    return instance_mock(str, request)
 
 
 class DescribePackage(object):
@@ -215,6 +115,88 @@ class DescribePackage(object):
         package.substitute_item(pkg_item_2_)
         # verify -----------------------
         assert pkg_item_.blob == 'new blob'
+
+    # fixtures -------------------------------------------------------------
+
+    @pytest.fixture
+    def blob_(self, request):
+        return instance_mock(str, request)
+
+    @pytest.fixture
+    def blob_2_(self, request):
+        return instance_mock(str, request)
+
+    @pytest.fixture
+    def blob_collection_(self, request, uri_, uri_2_, blob_, blob_2_):
+        return {uri_: blob_, uri_2_: blob_2_}
+
+    @pytest.fixture
+    def Package_(self, request):
+        Package_ = class_mock('opcdiag.model.Package', request)
+        return Package_
+
+    @pytest.fixture
+    def path_(self, request):
+        return instance_mock(str, request)
+
+    @pytest.fixture
+    def PhysPkg_(self, request, phys_pkg_):
+        PhysPkg_ = class_mock('opcdiag.model.PhysPkg', request)
+        PhysPkg_.read.return_value = phys_pkg_
+        return PhysPkg_
+
+    @pytest.fixture
+    def phys_pkg_(self, request, root_uri_, uri_, uri_2_, blob_, blob_2_):
+        phys_pkg = instance_mock(PhysPkg, request)
+        phys_pkg.root_uri = root_uri_
+        phys_pkg.__iter__.return_value = iter(((uri_,   blob_),
+                                               (uri_2_, blob_2_)))
+        return phys_pkg
+
+    @pytest.fixture
+    def PkgItem_(self, request, pkg_item_, pkg_item_2_):
+        PkgItem_ = class_mock('opcdiag.model.PkgItem', request)
+        PkgItem_.side_effect = [pkg_item_, pkg_item_2_]
+        return PkgItem_
+
+    @pytest.fixture
+    def pkg_item_(self, request, blob_):
+        pkg_item_ = instance_mock(PkgItem, request)
+        pkg_item_.blob = blob_
+        pkg_item_.is_rels_item = True
+        pkg_item_.is_xml_part = True
+        return pkg_item_
+
+    @pytest.fixture
+    def pkg_item_2_(self, request, blob_2_):
+        pkg_item_2_ = instance_mock(PkgItem, request)
+        pkg_item_2_.blob = blob_2_
+        pkg_item_2_.is_rels_item = False
+        pkg_item_2_.is_xml_part = False
+        return pkg_item_2_
+
+    @pytest.fixture
+    def pkg_item_3_(self, request):
+        pkg_item_3_ = instance_mock(PkgItem, request)
+        pkg_item_3_.is_rels_item = True
+        pkg_item_3_.is_xml_part = True
+        return pkg_item_3_
+
+    @pytest.fixture
+    def pkg_item_dict_(self, request, uri_, uri_2_, pkg_item_, pkg_item_2_):
+        return {uri_: pkg_item_, uri_2_: pkg_item_2_}
+
+    @pytest.fixture
+    def root_uri_(self, request):
+        return instance_mock(str, request)
+
+    @pytest.fixture
+    def uri_(self, request):
+        return instance_mock(str, request)
+
+    @pytest.fixture
+    def uri_2_(self, request):
+        return instance_mock(str, request)
 
 
 class DescribePkgItem(object):
